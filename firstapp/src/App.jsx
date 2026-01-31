2b200c5228272c07c74dda67be4c66426f394820
Charishma-Guduru-123 <charishmaguduru@gmail.com>
2026-01-31 15:12:50 +0530

    SDWEDSX

--- firstapp/src/App.jsx
+++ firstapp/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import Home from "./components/home.jsx";
+import Home from "./components/Home.jsx";
 import Register from "./components/Register.jsx";
 import Login from "./components/Login.jsx";
 import Navigation from "./components/Navigation.jsx";
