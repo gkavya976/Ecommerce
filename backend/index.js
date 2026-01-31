7f4bbdd937d40b561c8eb0fb54f1f3d6709de89e
Charishma-Guduru-123 <charishmaguduru@gmail.com>
2026-01-31 11:20:32 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors =require("cors")
 const mongoose = require("mongoose")
 require("dotenv").config() 
 const authRoutes=require("./routes/auth.js")
-const productRoutes=require("./routes/product.js")
+const productRoutes=require("./routes/Product.js")
 const cartRoutes=require("./routes/cart.js")
 const app = express() 
 const port=4000
